5bbc33a0f0df421e63a1be3817b85e4d8892e3cd
Kwasi O-T <tweneboah21@gmail.com>
2023-08-23 22:55:26 +0000

    updated app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-// import logo from './logo.svg';
+import React from "react";
 import './App.css';
+
+const picture = <img src="../kwasi-nobg.png" alt="personalPic" />
+
+const summary = <p>Budding frontend developer with a strong background of 10+ years experience building, managing and maintaining visually appealing, functional and user friendly websites, using Joomla and WordPress. Proficient in HTML, CSS and JavaScript. Also have good working knowledge in AWS cloud technologies, Git and GitHub.</p>
+
+const skills = <ul className="skills">
+  <li className="li">WordPress</li>
+  <li className="li">Joomla</li>
+  <li className="li">HTML</li>
+  <li className="li">CSS</li>
+  <li className="li">JavaScript</li>
+  <li className="li">Git</li>
+  <li className="li">GitHub</li>
+  <li className="li">AWS</li>
+</ul>
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <p>I am <mark>Kwasi Ofori - Tweneboah</mark>, this is my first React application.</p>
+    <div className="myApp">
+      <h1>My Portfolio</h1>
+      <div><hr /></div>
+      <div className="top">
+        <div>
+          {picture}
+        </div>
+        <span className="divider"></span>
+        <div className="bio">
+          <h2>Kwasi Ofori - Tweneboah</h2>
+          <p>Frontend Developer</p>
+          {summary}
+        </div>
+      </div>
+      <div><hr /></div>
+      <div className="middle">
+        <h3>Skills: {skills}</h3>
+      </div>
+      <div><hr /></div>
     </div>
   );
 }
